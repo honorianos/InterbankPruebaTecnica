a313de589f605768dda1ce8caf4090fcfe875dd4
Oswaldo Escobar Huisa <oescobar@company.com>
2023-04-23 11:20:40 -0500

    Initial Commit

---
+++ InterbankPruebaTecnica/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InterbankPruebaTecnica
+//
+//  Created by Oswaldo Escobar on 23/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
